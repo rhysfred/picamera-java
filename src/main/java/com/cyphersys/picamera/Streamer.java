e54526f695f89b9a2bf76a19a725380fcc41dfd0
rhys <rhys@frontleftspeaker.com>
2024-10-18 22:47:45 +1100

    Skeleton Java Streamer

--- src/main/java/com/cyphersys/picamera/Streamer.java
+++ src/main/java/com/cyphersys/picamera/Streamer.java
@@ -1,5 +1,23 @@
 package com.cyphersys.picamera;
 
+import java.lang.foreign.FunctionDescriptor;
+import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
+
 public class Streamer {
     
+    public Streamer() {
+        FunctionDescriptor frameHandlerDescription =
+        FunctionDescriptor.of(
+            ValueLayout.JAVA_BOOLEAN,
+            ValueLayout.ADDRESS.withTargetLayout(ValueLayout.JAVA_INT),
+            ValueLayout.JAVA_INT);
+    }
+
+    public Boolean handleFrame(MemorySegment buffer, MemorySegment size) {
+        Integer iSize = size.get(ValueLayout.JAVA_INT, 0);
+
+        
+        return true;
+    }
 }
